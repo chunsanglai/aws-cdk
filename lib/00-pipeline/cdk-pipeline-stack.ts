7fbc9048586d57fbd42027fd937723764af62089
Chun Lai <info@chunops.com>
2023-05-04 12:28:50 +0200

    add stage to prod

--- lib/00-pipeline/cdk-pipeline-stack.ts
+++ lib/00-pipeline/cdk-pipeline-stack.ts
@@ -19,7 +19,7 @@ export class PipelineStack extends Stack {
                         "arn:aws:codestar-connections:eu-central-1:788527717058:connection/79110d73-a381-41db-9a36-d59e06d05adb"
                 }
             ),
-            installCommands: ["npm install -g aws-cdk"],
+            // installCommands: ["npm install -g aws-cdk"],
             commands: ["npm ci", "npm run build", "npx cdk synth"]
         })
 
